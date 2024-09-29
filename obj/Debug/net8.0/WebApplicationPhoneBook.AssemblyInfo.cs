4ba5c11389ce0afa7f266a60a6b1e875340703fc
Aliaksei Dziatlau <aliakseidziatlau@gmail.com>
2024-09-29 23:21:10 +0200

    map start

--- obj/Debug/net8.0/WebApplicationPhoneBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplicationPhoneBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationPhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc99f5fc018c064ffecbdb43e4a829a93a8aa90e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196457a1a4ed9abaf532306d1a5ed2b277afd4e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationPhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationPhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
