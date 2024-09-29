fc99f5fc018c064ffecbdb43e4a829a93a8aa90e
Aliaksei Dziatlau <aliakseidziatlau@gmail.com>
2024-09-29 17:41:36 +0200

    JSON for cities

--- obj/Debug/net8.0/WebApplicationPhoneBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplicationPhoneBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationPhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d63d644a3cf28758a0b1949aff76fb74447b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f27483918589e986a212b7835a5e04999c73cda")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationPhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationPhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
